428e808e2046d81966bfd9a99ff3beaf34abf369
jaredpek <83465527+jaredpek@users.noreply.github.com>
2023-04-27 18:05:00 +0800

    update

--- project/order/views.py
+++ project/order/views.py
@@ -83,28 +83,53 @@ class UpdateOrderDetails(generics.RetrieveUpdateDestroyAPIView):
     queryset = Order.objects.all()
     serializer_class = OrderSerializer
 
+    def get_location(self, postal_code, key):
+        url = 'http://dev.virtualearth.net/REST/v1/Locations'
+        params = {
+            'countryRegion': 'SG',
+            'postalCode': postal_code,
+            'key': key,
+        }
+        coordinates = requests.get(url=url, params=params).json()['resourceSets'][0]['resources'][0]['geocodePoints'][0]['coordinates']
+        return coordinates
+
+    def get_distance(self, start, end, key):
+        url = 'http://dev.virtualearth.net/REST/v1/Routes'
+        params = {
+            'wp.0': f'{start[0]},{start[1]}',
+            'wp.1': f'{end[0]},{end[1]}',
+            'key': key,
+        }
+        distance = requests.get(url=url, params=params).json()['resourceSets'][0]['resources'][0]['routeLegs'][0]['routeSubLegs'][0]['travelDistance']
+        return distance
+
     def put(self, request, *args, **kwargs):
         data = self.request.data
         order_serializer = OrderSerializer(data=data)
         order_id = int(self.kwargs['pk'])
         order = Order.objects.get(id=order_id)
-        if order_serializer.is_valid(raise_exception=True):
-            new_status = int(data.get('status'))
-            if order.status == 0:
-                return super().put(request, *args, **kwargs)
-            elif 0 <= new_status and new_status <= 3 and new_status > order.status:
-                order.status = new_status
-                order.save()
-                return response.Response({'Success': 'Successfully updated order status'}, status.HTTP_202_ACCEPTED)
-            return response.Response({'Error': 'Not allowed to modify a confirmed order'}, status.HTTP_400_BAD_REQUEST)
-        return response.Response({'Error': order_serializer.errors}, status.HTTP_400_BAD_REQUEST)
+        if order.user == self.request.user:
+            if order_serializer.is_valid(raise_exception=True):
+                new_status = int(data.get('status'))
+                if order.status == 0:
+                    return super().put(request, *args, **kwargs)
+                elif 0 <= new_status and new_status <= 3 and new_status > order.status:
+                    order.status = new_status
+                    order.save()
+                    return response.Response({'Success': 'Successfully updated order status'}, status.HTTP_202_ACCEPTED)
+                return response.Response({'Error': 'Not allowed to modify a confirmed order'}, status.HTTP_400_BAD_REQUEST)
+            return response.Response({'Error': order_serializer.errors}, status.HTTP_400_BAD_REQUEST)
+        return response.Response({'Error': 'You are not allowed to modify this order'}, status.HTTP_401_UNAUTHORIZED)
     
     def destroy(self, request, *args, **kwargs):
         order_id = int(self.kwargs['pk'])
         order = Order.objects.get(id=order_id)
-        if order.status == 0:
-            return super().destroy(request, *args, **kwargs)
-        return response.Response({'Error': 'Not allowed to cancel a confirmed order'}, status.HTTP_400_BAD_REQUEST)
+        if order.user == self.request.user:
+            if order.status == 0:
+                order.delete()
+                return response.Response({'Success': 'Successfully deleted an order'}, status.HTTP_200_OK)
+            return response.Response({'Error': 'Not allowed to cancel a confirmed order'}, status.HTTP_400_BAD_REQUEST)
+        return response.Response({'Error': 'You are not allowed to modify this order'}, status.HTTP_401_UNAUTHORIZED)
 
 class OrderItemDetails(generics.ListCreateAPIView):
     queryset = OrderItem.objects.all()
@@ -121,24 +146,26 @@ class OrderItemDetails(generics.ListCreateAPIView):
         orderitem_serializer = CreateOrderItemSerializer(data=data)
         if orderitem_serializer.is_valid(raise_exception=True):
             order = Order.objects.get(id=data.get('order'))
-            if order.status == 0:
-                item = Item.objects.get(id=data.get('item'))
-                if order.restaurant == item.restaurant:
-                    quantity = data.get('quantity')
-                    total_cost = round(float(quantity) * item.price, 2)
-                    OrderItem.objects.create(
-                        order=order,
-                        item=item,
-                        price=item.price,
-                        quantity=quantity,
-                        total_cost=total_cost,
-                    )
-                    order.items_subtotal += total_cost
-                    order.total_cost += total_cost
-                    order.save()
-                    return response.Response({'Success': 'Item added to order successfully'}, status.HTTP_201_CREATED)
-                return response.Response({'Error': 'Invalid item selected'}, status.HTTP_400_BAD_REQUEST)
-            return response.Response({'Error': 'Not allowed to modify a confirmed order'}, status.HTTP_400_BAD_REQUEST)
+            if order.user == self.request.user:
+                if order.status == 0:
+                    item = Item.objects.get(id=data.get('item'))
+                    if order.restaurant == item.restaurant:
+                        quantity = data.get('quantity')
+                        total_cost = round(float(quantity) * item.price, 2)
+                        OrderItem.objects.create(
+                            order=order,
+                            item=item,
+                            price=item.price,
+                            quantity=quantity,
+                            total_cost=total_cost,
+                        )
+                        order.items_subtotal += total_cost
+                        order.total_cost += total_cost
+                        order.save()
+                        return response.Response({'Success': 'Item added to order successfully'}, status.HTTP_201_CREATED)
+                    return response.Response({'Error': 'Invalid item selected'}, status.HTTP_400_BAD_REQUEST)
+                return response.Response({'Error': 'Not allowed to modify a confirmed order'}, status.HTTP_400_BAD_REQUEST)
+            return response.Response({'Error': 'You are not allowed to modify this order'}, status.HTTP_401_UNAUTHORIZED)
         return response.Response({'Error': orderitem_serializer.errors}, status.HTTP_400_BAD_REQUEST)
     
 class UpdateOrderItemDetails(generics.RetrieveUpdateDestroyAPIView):
